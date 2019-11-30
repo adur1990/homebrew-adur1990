83558f3ab11f162cb5af92d92ae86fa6c91c480c
Artur <git@artursterz.de>
2019-11-30 21:33:58 +0100

    Passafari version bump

--- Casks/passafari.rb
+++ Casks/passafari.rb
@@ -1,6 +1,6 @@
 cask 'passafari' do
-  version '0.4'
-  sha256 'ebfb25ea8c4fc21a7e070f16f9bee94997d288255e85f9215d86abb39502c4a0'
+  version '0.5'
+  sha256 '84a37be6331019180931274138390121fbf684b8bcf1c4f80d284a19be9da223'
 
   url "https://github.com/adur1990/passafari/releases/download/#{version}/Passafari.app.zip"
   name 'Passafari'
