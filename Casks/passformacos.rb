4801c71791e2bd61ca6d7929771fb181f4cd96fd
Artur <github@artursterz.de>
2020-02-02 20:15:08 +0100

    Update Pass for macOS formular to version 0.6.1

--- Casks/passformacos.rb
+++ Casks/passformacos.rb
@@ -2,8 +2,8 @@ cask 'passformacos' do
   depends_on formula: 'pass'
   depends_on macos: '>= :mojave'
 
-  version '0.6'
-  sha256 '488994fd25e9fb477925b0f282091ce2f5cae0deecd1e3000d35386df8d42f3b'
+  version '0.6.1'
+  sha256 'fc9ab1ea2ec1d89115a03a4e60b836ed63b84ad2499a14dfa9fb024ea2917e63'
 
   url "https://github.com/adur1990/Pass-for-macOS/releases/download/#{version}/Pass.for.macOS.app.zip"
   name 'Pass for macOS'
