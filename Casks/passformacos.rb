3215ffe4e1262c987594178e86b9eed0bfd650c1
Github Action <github@artursterz.de>
2021-01-24 12:14:50 +0000

    Update Pass for macOS to version v0.9 (automated commit)

--- Casks/passformacos.rb
+++ Casks/passformacos.rb
@@ -2,8 +2,8 @@ cask 'passformacos' do
  	depends_on formula: 'pass'
  	depends_on macos: '>= :mojave'
  
- 	version 'v0.8.2'
- 	sha256 '3fff8f893b1b4e39f003ceaa86100e7883ade9f60a6f2ef4aff087d17191d216'
+ 	version 'v0.9'
+ 	sha256 '23d17b218acbf774b511b7842a0e02a256aea8dbecf26e72636973fe6478b94f'
  
  	url "https://github.com/adur1990/Pass-for-macOS/releases/download/#{version}/Pass.for.macOS.app.zip"
  	name 'Pass for macOS'
